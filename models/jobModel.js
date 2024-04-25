a7ac247192dc052d9f2d23bbc086055d2ac21a72
Your Name <you@example.com>
2024-04-26 05:09:22 +0700

    added delete

--- models/jobModel.js
+++ models/jobModel.js
@@ -2,9 +2,11 @@
 const mongoose = require('mongoose');
 
 const jobSchema = new mongoose.Schema({
-  position: { type: String, required: true },
+  position: { type: String },
   company: { type: String, required: true },
   status: { type: String, required: true },
+  url: { type: String, required: true },
+  address: { type: String, required: true },
   dateSaved: { type: Date, default: Date.now },
   dateApplied: { type: Date },
   followUp: { type: Date }
