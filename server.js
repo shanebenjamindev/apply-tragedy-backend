af7cf360f3cbaa63db7848ade6b90e35eb383e9e
Your Name <you@example.com>
2024-04-29 09:21:48 +0700

    fixing dependencies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const mongoose = require("mongoose");
-const routes = require('./src/routes')
+const routes = require('./routes')
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
@@ -12,8 +12,8 @@ const app = express()
 const port = process.env.PORT || 3001
 
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(bodyParser.json())
 app.use(cookieParser())
 
@@ -24,7 +24,7 @@ mongoose.connect(`${process.env.MONGO_DB}`)
         console.log('Connect Db success!')
     })
     .catch((err) => {
-        console.log(err)
+        // console.log(err)
     })
 app.listen(port, () => {
     console.log('Server is running in port: ', + port)
