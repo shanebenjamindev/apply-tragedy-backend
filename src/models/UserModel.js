dd56e73d16ac9cfba6bff4ca5a6bbea098aface8
Your Name <you@example.com>
2024-05-02 15:51:33 +0700

    update usermodal

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema(
     {
+        avatar: { type: String },
         email: { type: String, required: true, unique: true },
         firstName: { type: String, required: true, },
         lastName: { type: String, required: true, },
